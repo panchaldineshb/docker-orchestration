cbf228a8b54009796198abb3aa091a892c586e4d
Dinesh Panchal <dpanchal@usga.org>
2018-07-17 20:19:37 -0400

    DP:Added changes

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ log.warning("This is a warning.")
 log.error("This is an error.")
 
 
-client = MongoClient('mongodb://sarabi:27017/')
+client = MongoClient('mongodb://0.0.0.0:27017')
 log.info(client)
 log.info("Connection Successful")
-db = client.sarabi
+db = client.local
 log.info(db)
 
 
